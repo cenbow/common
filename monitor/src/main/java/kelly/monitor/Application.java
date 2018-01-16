02d59d7c686def4592b1e259061329cdd47eb9a6
kelly.li <kelly.li@qunar.com>
2018-01-17 00:30:17 +0800

    update

--- monitor/src/main/java/kelly/monitor/Application.java
+++ monitor/src/main/java/kelly/monitor/Application.java
@@ -3,22 +3,22 @@ package kelly.monitor;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by kelly.li on 17/9/16.
  */
 
 @SpringBootApplication
-@EnableCaching
-public class Application{
-        //extends SpringBootServletInitializer
-        // {
+//@EnableCaching
+public class Application
+        extends SpringBootServletInitializer {
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
