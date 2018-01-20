410762b081ba3b654344b12f07bf94fa31db998c
kelly.li <kelly.li@qunar.com>
2018-01-20 20:42:31 +0800

    update

--- music163/src/main/java/kelly/music163/SpiderTask.java
+++ music163/src/main/java/kelly/music163/SpiderTask.java
@@ -30,9 +30,7 @@ public class SpiderTask {
         try {
             //私钥，随机16位字符串（自己可改）
             String secKey = "cd859f54539b24b7";
-            //303263108
-            //6858713
-            String text = "{\"username\": \"\", \"rememberLogin\": \"true\", \"password\": \"\",\"uid\": \"6858713\",\"type\": \"1\"}";
+            String text = "{\"username\": \"\", \"rememberLogin\": \"true\", \"password\": \"\",\"uid\": \"303263108\",\"type\": \"1\"}";
             String modulus = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7";
             String nonce = "0CoJUm6Qyw8W8jud";
             String pubKey = "010001";
